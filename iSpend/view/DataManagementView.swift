fa23f9fa1672447a7988f71fc69ef93762233d52
Spencer Marks <spencer.marks@childrens.harvard.edu>
2024-08-27 19:49:43 -0400

    lots of config

--- iSpend/view/DataManagementView.swift
+++ iSpend/view/DataManagementView.swift
@@ -1,11 +1,5 @@
-//
-//  DataManagementView.swift
-//  iSpend
-//
-//  Created by Spencer Marks on 8/27/24.
-//
-
 import Foundation
+import SwiftData
 import SwiftUI
 
 struct DataManagementView: View {
@@ -14,80 +8,40 @@ struct DataManagementView: View {
 
     var expenses: [ExpenseModel]
 
-    @State var isPresentingConfirm: Bool = false
+    @State private var isPresentingConfirm: Bool = false
     @State private var showAlert = false
+    @State private var showErrorAlert = false
+    @State private var errorMessage: String = ""
 
-    var exportButtonLabel: String {
-        if expenses.isEmpty {
-            "Export (No data to export)"
-        } else {
-            "Export"
-        }
+    private var exportButtonLabel: String {
+        expenses.isEmpty ? "Export (No data to export)" : "Export"
     }
 
     var body: some View {
         NavigationView {
             List {
-                Button("Reset all expense data", role: .destructive) {
-                    isPresentingConfirm = true
-                    
-                }.confirmationDialog("Are you sure?",
-                                     isPresented: $isPresentingConfirm) {
-                    Button("Delete all expenses?", role: .destructive) {
-                        do {
-                            try modelContext.delete(model: ExpenseModel.self)
-                        } catch {
-                            print("Failed to clear all ExpenseModel data.")
-                        }
-                    }
-                }
+                resetButton(title: "Reset all expense data", modelTypes: [ExpenseModel.self], errorMessage: "Failed to clear all ExpenseModel data")
                 
-                Button("Reset budget data", role: .destructive) {
-                    isPresentingConfirm = true
-                }.confirmationDialog("Are you sure?",
-                                     isPresented: $isPresentingConfirm) {
-                    Button("Delete all budget data restore defaults?", role: .destructive) {
-                        do {
-                            try modelContext.delete(model: BudgetModel.self)
-                        } catch {
-                            print("Failed to clear all BudgetModel data.")
-                        }
-                    }
-                }
+                resetButton(title: "Reset budget data", modelTypes: [BudgetModel.self], errorMessage: "Failed to clear all BudgetModel data.")
                 
-                Button("Delete all data", role: .destructive) {
-                    isPresentingConfirm = true
-                }.confirmationDialog("Are you sure?",
-                                     isPresented: $isPresentingConfirm) {
-                    Button("Delete all budget data restore defaults?", role: .destructive) {
-                        do {
-                            try modelContext.delete(model: BudgetModel.self)
-                            try modelContext.delete(model: ExpenseModel.self)
-                            try modelContext.delete(model: CatergoriesModel.self)
-                            try modelContext.delete(model: MediationsModel.self)
-                        } catch {
-                            print("Failed to clear all  data.")
-                        }
-                    }
-                }
-                
+                resetButton(title: "Delete all data",
+                            modelTypes: [CatergoriesModel.self],
+                            errorMessage: "Failed to clear all data.")
+                  
                 Button(exportButtonLabel) {
                     let csvString = generateCSV(from: expenses)
                     UIPasteboard.general.string = csvString
                     print("CSV string copied to clipboard.")
                     showAlert = true
-                    
                 }.alert(isPresented: $showAlert) {
                     Alert(
-                        title: Text("\(expenses.count) exported "),
-                        message: Text("Your data is now in  ready to paste into a file. Save the file with a .csv extension and view in your favorite spreadsheet program"),
+                        title: Text("\(expenses.count) exported"),
+                        message: Text("Your data is now ready to paste into a file. Save the file with a .csv extension and view in your favorite spreadsheet program"),
                         dismissButton: .default(Text("OK"))
                     )
                 }.disabled(expenses.isEmpty)
-                
-                
-                
-            }.toolbar {
+            }
+            .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Done") {
                         dismiss()
@@ -96,7 +50,32 @@ struct DataManagementView: View {
             }
         }
     }
-}
+
+    private func resetButton<T: PersistentModel>(title: String, modelTypes: [T.Type], errorMessage: String) -> some View {
+        Button(title, role: .destructive) {
+            isPresentingConfirm = true
+        }
+        .confirmationDialog("Are you sure?", isPresented: $isPresentingConfirm) {
+            Button("Delete all data?", role: .destructive) {
+                do {
+                    for modelType in modelTypes {
+                        try modelContext.delete(model: modelType)
+                    }
+                    dismiss()
+                } catch {
+                    self.errorMessage = errorMessage
+                    showErrorAlert = true
+                }
+            }
+        }
+        .alert(isPresented: $showErrorAlert) {
+            Alert(
+                title: Text("Error"),
+                message: Text(self.errorMessage),
+                dismissButton: .default(Text("OK"))
+            )
+        }
+    }
 
 func generateCSV(from expenses: [ExpenseModel]) -> String {
     var csvString = "id,name,type,amount,note,date\n"
